de97cef5ac90f70cd1dd4d312997e6cb8db4c824
Alan Tibbetts <alan.tibbetts@hiq.se>
2016-09-13 14:07:08 +0200

    Load and display recent results. Tidy up the UI a little.

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -3,9 +3,20 @@ var randomSorterApp = angular.module("randomApp");
 var mainController = randomSorterApp.controller("mainController",
     function ($scope, randomSorterService) {
 
+        $scope.results = [];
+
+        var loadResults = function() {
+           randomSorterService.loadResults(function(data) {
+               $scope.results = data;
+           });
+        };
+
         $scope.sort = function(numbers) {
             console.log(numbers);
             randomSorterService.sort(numbers);
-        }
+            loadResults();
+        };
+
+        loadResults();
     }
 );
--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -4,14 +4,25 @@ randomSorterApp.factory('randomSorterService',
     function ($resource) {
 
         var sortResource = $resource('/api/sort');
+        var historyResource = $resource('/api/list?max=20');
 
-        var sortNumbers = function (numbers, success, error) {
+        var sortNumbers = function (numbers, fnSuccess, fnError) {
             sortResource.save(numbers);
         };
 
         return {
-            sort: function (numbers, success, error) {
-                sortNumbers(numbers, success, error);
+            sort: function (numbers, fnSuccess, fnError) {
+                sortNumbers(numbers, fnSuccess, fnError);
+            },
+
+            loadResults: function(fnSuccess, fnError) {
+                historyResource.query().$promise.then(
+                    function (data) {
+                        fnSuccess(data)
+                    },
+                    function (err) {
+                        fnError();
+                    });
             }
         }
     }
